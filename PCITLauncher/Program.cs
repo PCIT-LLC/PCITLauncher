4076aa5f1b62e1e04dd66ad85c0454569ddb4aaf
John James <jjames@phantomhost.net>
2026-09-14 10:51:16 -0500

    Add diagnostic logging to Program.cs

--- PCITLauncher/Program.cs
+++ PCITLauncher/Program.cs
@@ -3,6 +3,26 @@ using PCITLauncher;
 [STAThread]
 static void Main()
 {
-    ApplicationConfiguration.Initialize();
-    Application.Run(new MainForm());
+    try
+    {
+        var logPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "PCITLauncher", "startup.log");
+        Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+        File.AppendAllText(logPath, $"[{DateTime.Now}] Process started{Environment.NewLine}");
+
+        ApplicationConfiguration.Initialize();
+        File.AppendAllText(logPath, $"[{DateTime.Now}] ApplicationConfiguration.Initialize() OK{Environment.NewLine}");
+
+        Application.Run(new MainForm());
+        File.AppendAllText(logPath, $"[{DateTime.Now}] Application.Run() returned{Environment.NewLine}");
+    }
+    catch (Exception ex)
+    {
+        var logPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "PCITLauncher", "startup.log");
+        Directory.CreateDirectory(Path.GetDirectoryName(logPath)!);
+        File.AppendAllText(logPath, $"[{DateTime.Now}] FATAL: {ex}{Environment.NewLine}");
+    }
 }
